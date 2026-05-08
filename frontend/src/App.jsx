012c6538217404458ea1fb5971e344aa805d7d5b
operionosweb <operionosweb@gmail.com>
2026-05-08 10:25:08 +0200

    Add Auth route to App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Auth from "./pages/Auth";
 import ControlCenter from "./pages/ControlCenter";
 import Copilot from "./pages/Copilot";
 
 function App() {
+
   return (
     <Router>
 
       <Routes>
 
+        {/* AUTH ENTRY */}
+        <Route path="/auth" element={<Auth />} />
+
+        {/* MAIN APP */}
         <Route path="/" element={<ControlCenter />} />
-
         <Route path="/control-center" element={<ControlCenter />} />
-
         <Route path="/copilot" element={<Copilot />} />
 
       </Routes>
