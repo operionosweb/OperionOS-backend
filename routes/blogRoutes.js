dbe840591a537d6b247be9a74caf0413198f058d
operionosweb <operionosweb@gmail.com>
2026-05-25 08:50:44 +0200

    Create blogRoutes.js

---
+++ routes/blogRoutes.js
@@ -0,0 +1,50 @@
+import express from "express";
+
+import {
+  createPost,
+  getAllPosts,
+  getPostBySlug,
+  updatePost,
+  deletePost,
+  publishPost
+} from "../services/blogService.js";
+
+const router = express.Router();
+
+/* =====================================================
+   CREATE POST
+===================================================== */
+
+router.post("/", createPost);
+
+/* =====================================================
+   GET ALL POSTS
+===================================================== */
+
+router.get("/", getAllPosts);
+
+/* =====================================================
+   GET SINGLE POST
+===================================================== */
+
+router.get("/:slug", getPostBySlug);
+
+/* =====================================================
+   UPDATE POST
+===================================================== */
+
+router.put("/:id", updatePost);
+
+/* =====================================================
+   DELETE POST
+===================================================== */
+
+router.delete("/:id", deletePost);
+
+/* =====================================================
+   PUBLISH POST
+===================================================== */
+
+router.post("/:id/publish", publishPost);
+
+export default router;
