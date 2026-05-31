15f881cae3886b5d32fb030e77d25764694a42ff
operionosweb <operionosweb@gmail.com>
2026-05-31 11:45:26 +0200

    Refactor blogRoutes to use publishPost from controller

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -6,45 +6,16 @@ import {
   getPostBySlug,
   updatePost,
   deletePost,
-  publishPost
-} from "../services/blogService.js";
+  publishPost,
+} from "../controllers/blogController.js";
 
 const router = express.Router();
 
-/* =====================================================
-   CREATE POST
-===================================================== */
-
 router.post("/", createPost);
-
-/* =====================================================
-   GET ALL POSTS
-===================================================== */
-
 router.get("/", getAllPosts);
-
-/* =====================================================
-   GET SINGLE POST
-===================================================== */
-
 router.get("/:slug", getPostBySlug);
-
-/* =====================================================
-   UPDATE POST
-===================================================== */
-
 router.put("/:id", updatePost);
-
-/* =====================================================
-   DELETE POST
-===================================================== */
-
 router.delete("/:id", deletePost);
-
-/* =====================================================
-   PUBLISH POST
-===================================================== */
-
 router.post("/:id/publish", publishPost);
 
 export default router;
